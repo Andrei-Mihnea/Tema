3ad65b25c8101ea8db595a51134f351e17725919
Andrei-Mihnea <andrei.mihnea@student.tuiasi.ro>
2024-12-01 02:53:20 +0200

    Final hw(improvments soon?)

--- client.py
+++ client.py
@@ -1,4 +1,22 @@
 import socket,time
+import threading
+
+def recv_msg(sock):
+    while 1:
+        try:
+            data = sock.recv(1024).decode("ascii")
+
+            if not data:
+                print("Connection closed from server")
+                break
+
+            print(data)
+
+        except Exception as e:
+            pass
+            break
+    sock.close()
+
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
@@ -6,20 +24,24 @@ s.connect(('127.0.0.1',5000))
 info = s.recv(1024).decode("ascii")
 
 print(info)
+
+threading.Thread(target=recv_msg,args=(s,),daemon=True).start()
+
 try:
     while 1:
+        try:
+            command = input()
+            s.sendall(bytes(command,encoding="ascii"))
 
-        command = input()
-        s.sendall(bytes(command,encoding="ascii"))
-
-        data = s.recv(1024).decode("ascii")
-        print(f"{str(data)}")
-
-        if command == "--EXIT":
+            if command == "--EXIT":
+                break
+        except OSError:
+            print("Connection interrupted")
             break
 
         time.sleep(1)
 except KeyboardInterrupt:
     print("\nDisconnecting client")
+
 s.close()
 
--- server.py
+++ server.py
@@ -12,6 +12,8 @@ def comm_thread(conn,addr):
 
     with clients_lock:
         clients[conn] = f"{addr[1]}"
+
+    check = False
     try:
         while 1:
             try:
@@ -19,28 +21,36 @@ def comm_thread(conn,addr):
                 if not data:
                     break
                 command = str(data)
-                if command.startswith("--EXIT"):
+
+                if not check and not command.upper().startswith("--NAME"):
+                        conn.sendall(bytes("Please use --NAME=(username) command to register your name",encoding="ascii"))
+                elif command.upper().startswith("--EXIT"):
                     print(f"User with name {clients[conn]} has disconnected from the server")
-                    conn.sendall(bytes(f"User with name {clients[conn]} has disconnected from the server",encoding="ascii"))
+                    for connection in clients.keys():
+                        if connection != conn:
+                            connection.sendall(bytes(f"User with name {clients[conn]} has disconnected from the server",encoding="ascii"))
+                        conn.sendall(bytes(f"User {clients[conn]} disconnected from server",encoding="ascii"))
+                    with clients_lock:
+                        del clients[conn]
                     break
                     
-                elif command.startswith("--NAME"):
+                elif command.upper().startswith("--NAME"):
                     new_name = command[7:].lower()
-
+                    check = True
                     with clients_lock:
                         if new_name in clients.values():
                             print("Error")
                             conn.sendall(bytes("Operation failed\nName already taken, try something else\nPlease change your name using the command --NAME=(your name)",encoding="ascii"))
                         else:
                             print(f"{clients[conn]} changed its name to {str(new_name)}")
-                            conn.sendall(bytes(str(clients[conn]) + 'changed its name to ' + str(new_name), encoding="ascii"))
+                            conn.sendall(bytes(str(clients[conn]) + ' changed its name to ' + str(new_name), encoding="ascii"))
                             clients[conn] = new_name
 
                 elif command.startswith("--DISP"):
-                        print(clients)
+                        print(clients.values())
                         conn.sendall(bytes("Data printed to server",encoding="ascii"))
 
-                elif command.startswith("--CLOSE"):
+                elif command.upper().startswith("--CLOSE"):
                     with clients_lock:
                         username = str(command[8:].lower())
                         if clients[conn] == "admin" and username != "admin":
@@ -51,8 +61,12 @@ def comm_thread(conn,addr):
                                     break
                             if target_conn:
                                 print(f"User {username} disconnected by admin")
+
+                                for connection in clients.keys():
+                                    if connection != target_conn:
+                                        connection.sendall(bytes(f"User {username} got its connection closed by the admin",encoding="ascii"))
+
                                 target_conn.sendall(bytes("You have been disconnected by the admin",encoding="ascii"))
-                                conn.sendall(bytes(f"{clients[target_conn]} will be disconnected",encoding="ascii"))
                                 target_conn.close()
                                 del clients[target_conn] 
                             else:
@@ -65,22 +79,17 @@ def comm_thread(conn,addr):
 
                     
                 else:
-                    conn.sendall(bytes("Unkown command, try use help to get a list of all commands"))
-                    
+                    conn.sendall(bytes("Unkown command, try use help to get a list of all commands"))          
                 print("____________________________________________")
-            except ConnectionResetError:
-                    print(f"Connection interrupted abruptly from {clients.get(conn,addr[1])}")
-                    break
-            except Exception as e:
-                print(f"Error: {e}")
-                break
-    except WindowsError:
-        with clients_lock:
-            if conn in clients:
-                print(f"Deleting traces for client {clients[conn]}")
-                del clients[conn]
-        conn.close()
-        print("Connection closed")
+            except (ConnectionResetError,OSError,ConnectionAbortedError):
+                    with clients_lock:
+                        print(f"Connection ended abruptly by {clients[conn]}")
+                        break
+    except Exception as e:        
+            print(f"Unexpected error {e}")
+
+    conn.close()
+    
             
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
